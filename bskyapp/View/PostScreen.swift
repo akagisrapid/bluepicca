3e894534850e80cfbb0e14babd2b0589ed6a1eda
shuya <akagisrapid@gmail.com>
2024-03-26 11:41:18 +0900

    文字数制御

--- bskyapp/View/PostScreen.swift
+++ bskyapp/View/PostScreen.swift
@@ -4,11 +4,23 @@ struct PostScreen: View {
     @StateObject var screenModel: PostScreenModel
     @State var isPostCompleted:Bool = false
     @State var isPostFailed:Bool = false
+    @State var isTextValid: Bool = true
+    var maxTextCount: Int = 300
     var body: some View {
         VStack{
             Text("hello world")
             TextField("つぶやきたいこと", text:$screenModel.text)
                 .textFieldStyle(.roundedBorder)
+                .border(isTextValid ? Color.green : Color.red)
+                .onChange(of: screenModel.text) {
+                    // ユーザー名のバリデーション
+                    isTextValid = 0 < screenModel.text.count && screenModel.text.count <= maxTextCount
+                }
+            Text("\(screenModel.text.count) / \(maxTextCount)")
+            if !isTextValid {
+                Text(screenModel.text.count == 0 ? "何か言うことはないか": "言いたいことが多すぎる")
+            }
+                
             
             Button("送信") {
                 Task{
