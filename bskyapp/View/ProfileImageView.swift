12253f079ef564780a13f4374961e879f91836c6
shuya <akagisrapid@gmail.com>
2026-01-20 20:14:03 +0900

    structに変更

--- bskyapp/View/ProfileImageView.swift
+++ bskyapp/View/ProfileImageView.swift
@@ -1,27 +1,27 @@
 import SwiftUI
 
 struct ProfileImageView: View {
-    @StateObject var viewModel: AsyncImageViewModel
-    @State var actor: String 
-    @State var isProfileView = false
-    var body: some View {
-        ZStack{
-            // サムネ色
-            AsyncImage(url: viewModel.url){ image in
-                image.image?
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: viewModel.imageSize.maxWidth, maxHeight: viewModel.imageSize.maxHeight)
-            }
-            .gesture(
-                TapGesture().onEnded{
-                    isProfileView = true
-                }
-            )
-            
-            .sheet(isPresented: $isProfileView){
-                ProfileView(viewModel: .init(actor: actor, profile: .init(did: "", handle: "", labels: [])))
-            }
+  let viewModel: AsyncImageViewModel
+  @State var actor: String
+  @State var isProfileView = false
+  var body: some View {
+    ZStack {
+      // サムネ色
+      AsyncImage(url: viewModel.url) { image in
+        image.image?
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(maxWidth: viewModel.imageSize.maxWidth, maxHeight: viewModel.imageSize.maxHeight)
+      }
+      .gesture(
+        TapGesture().onEnded {
+          isProfileView = true
         }
+      )
+
+      .sheet(isPresented: $isProfileView) {
+        ProfileView(viewModel: .init(actor: actor, profile: .init(did: "", handle: "", labels: [])))
+      }
     }
+  }
 }
